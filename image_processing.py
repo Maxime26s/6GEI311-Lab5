4bf39bd804778f334d408b0c0d45790ce3f8ba9a
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2021-11-15 12:19:01 -0500

    init

---
+++ image_processing.py
@@ -0,0 +1,156 @@
+import math
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter
+from collections import deque
+from time import time
+import cv2
+from PIL import Image
+from scipy.signal import convolve2d
+from numba import jit
+
+
+class ImageProcessing:
+    def __init__(
+        self,
+        diff_threshold=5,
+        scale_ratio=0.2,
+        compression_ratio=0.1,
+        bg_buffer_size=10,
+        motion_buffer_size=2,
+    ):
+        self.diff_threshold = diff_threshold
+        self.scale_ratio = scale_ratio
+        self.compression_ratio = compression_ratio
+
+        self.bg_buffer_size = bg_buffer_size
+        self.motion_buffer_size = motion_buffer_size
+
+        self.bg_buffer = deque(maxlen=bg_buffer_size)
+        self.motion_buffer = deque(maxlen=motion_buffer_size)
+        self.orig_frames = deque(maxlen=motion_buffer_size)
+        self.bg_sum = None
+        self.color_movement = None
+        self.detection = None
+
+    def testGaussian(self, image):
+        img = []
+        for d in range(3):
+            img.append(
+                convolve2d(image[:, :, d], np.ones((5, 5)) * 1 / 25, "same", "symm"),
+            )
+        im_conv = np.stack(img, axis=2).astype("uint8")
+        return im_conv
+
+    def resize_cv(self, image, coefficient):
+        return cv2.resize(
+            image,
+            (int(image.shape[1] * coefficient), int(image.shape[0] * coefficient)),
+            interpolation=cv2.INTER_CUBIC,
+        )
+
+    def resize_image_rgb(self, image, coefficient):
+        test = np.array(
+            Image.fromarray(image).resize(
+                (int(image.shape[1] * coefficient), int(image.shape[0] * coefficient))
+            )
+        )
+
+        h, w, d = image.shape
+        mult = math.ceil(1 / coefficient)
+        image = (
+            image.reshape(
+                math.floor(h * coefficient), mult, math.floor(w * coefficient), mult, d
+            )
+            .max(3)
+            .max(1)
+        )
+        return image
+
+    def resize_image_gray(self, image, coefficient):
+        h, w = image.shape
+        mult = math.ceil(1 / coefficient)
+        return (
+            image.reshape(
+                1, math.floor(h * coefficient), mult, math.floor(w * coefficient), mult
+            )
+            .max(4)
+            .max(2)
+            .max(0)
+        )
+
+    def prepare_image(self, image):
+        img = image
+        img = self.resize_cv(img, self.scale_ratio)
+        # img = self.testGaussian(img)
+        img = cv2.GaussianBlur(img, (5, 5), 0)
+        # img = gaussian_filter(img, sigma)
+        return img
+
+    def substract_image(self, img1, img2):
+        diff = np.abs(img2 - img1)
+        return diff
+
+    def apply_diff_threshold(self, image):
+        image[image < self.diff_threshold] = 0
+        image[image >= self.diff_threshold] = 255
+        return image
+
+    def rgb2gray(self, image):
+        return np.mean(image, -1)
+
+    def process_image1(self, image):
+        img = self.prepare_image(image)
+        img_float = img.astype("float32")
+
+        self.add_to_background(img_float)
+        self.motion_buffer.append(img_float)
+
+        background_average = self.calc_background_average()
+        a = time()
+        movement_average = self.calc_weighted_movement_average(img_float.shape)
+        b = time()
+        print(b - a)
+
+        if len(self.bg_buffer):
+            movement = self.substract_image(movement_average, background_average)
+        else:
+            movement = np.zeros(movement_average.shape)
+        self.color_movement = movement
+
+        self.apply_diff_threshold(movement)
+        movement = movement.astype("uint8")
+        movement = self.rgb2gray(movement)
+        movement[movement > 0] = 254
+
+        self.detection = self.resize_cv(
+            movement, self.compression_ratio / self.scale_ratio
+        )
+
+        return self.color_movement
+
+    def calc_weighted_movement_average(self, movement_shape):
+        weighted_sum = np.zeros(movement_shape, dtype="float32")
+        i = 0
+        total = 0
+        for frame in self.motion_buffer:
+            i += 1
+            total += i
+            weighted_sum = weighted_sum + frame * i
+        return weighted_sum / total
+
+    def add_to_background(self, image):
+        if len(self.motion_buffer) == self.motion_buffer_size:
+            frame = self.motion_buffer[0]
+        else:
+            frame = image
+
+        if self.bg_sum is None:
+            self.bg_sum = np.zeros(frame.shape, dtype="float32")
+        elif len(self.bg_buffer) == self.bg_buffer_size:
+            self.bg_sum = self.bg_sum - self.bg_buffer[0]
+
+        self.bg_sum = self.bg_sum + frame
+        self.bg_buffer.append(frame)
+
+    def calc_background_average(self):
+        return self.bg_sum / len(self.bg_buffer)
---
+++ main.py
@@ -0,0 +1,96 @@
+import numpy as np
+import tkinter as tk
+import math
+from tkinter.filedialog import askopenfilename
+from PIL import Image, ImageTk
+from image_processing import ImageProcessing
+import cv2
+from imutils.video import FileVideoStream, VideoStream
+import threading
+from time import time
+
+image_processing = ImageProcessing()
+# https://www.pluralsight.com/guides/importing-image-data-into-numpy-arrays
+# Classe contenant l'application (tkinter)
+class Application(tk.Frame):
+    # Initialisation de la fenêtre
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+        self.master.geometry("1000x800")
+        self.label = None
+        self.count = 0
+        self.thread = None
+        self.last_frame_time = time()
+        self.vs = FileVideoStream(path="./cam2.mp4").start()
+        # self.vs = VideoStream(src=0).start()
+        self.thread = threading.Thread(target=self.video_loop, args=())
+        self.thread.start()
+
+    # Création de boutons
+    def create_widgets(self):
+        self.selectFile1 = tk.Button(self)
+        self.selectFile1["text"] = "Select files"
+        self.selectFile1["command"] = self.video_loop
+        self.selectFile1.pack(side="top")
+
+    # Fonction de sélection de fichier
+    def select_file(self, fileName=""):
+        if fileName == "":
+            fileName = askopenfilename()
+        return Image.open(fileName)
+
+    def select_two_images(self):
+        self.image1 = np.array(self.select_file("image1.png"))
+        self.image2 = np.array(self.select_file("image2.png"))
+        image = Image.fromarray(
+            image_processing.process_image(self.image1, self.image2)
+        )
+        self.display_image(image)
+
+    def video_loop(self):
+        while True:
+            while time() <= self.last_frame_time + 30 / 1000:
+                pass
+            self.last_frame_time = time()
+            self.frame = self.vs.read()
+            if self.frame is None:
+                break
+
+            image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+            image = image_processing.process_image1(image)
+            # image = image_processing.resize_cv(image, 1920 / image.shape[1])
+            image = Image.fromarray(image_processing.color_movement.astype("uint8"))
+            # image = image.resize(
+            #    (1920, int((float(image.size[1]) * float((1920 / float(image.size[0]))))))
+            # )
+            self.display_image(image)
+
+    def display_image(self, image1):
+        image = ImageTk.PhotoImage(image1)
+
+        if self.label is None:
+            self.label = tk.Label(image=image)
+            self.label.place(x=0, y=0)
+        else:
+            self.label.configure(image=image)
+        self.label.image = image
+
+        self.count += 1
+
+    def resize(self, image):
+        size = 1920, 1080
+        image.thumbnail(size, Image.ANTIALIAS)
+        return image
+
+    # Fonction pour fermer l'application
+    def quit(self):
+        self.master.destroy()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
