3581dc53fb13e2bfecb7409750b647bbf5fc3de7
Alan Brucher <brucher.alann@gmail.com>
2021-11-23 20:37:25 -0500

    Add interface v1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # pip install -r requirements.txt 
 import numpy as np
+from tkinter import *
 import tkinter as tk
+from tkinter import ttk
+import tkinter.font as font
+from functools import partial
 import math
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
@@ -11,48 +15,96 @@ import threading
 from time import time
 from image_acquisition import get_image
 import send_alert
-from functools import partial
 
 
 image_processing = ImageProcessing()
 # https://www.pluralsight.com/guides/importing-image-data-into-numpy-arrays
 # Classe contenant l'application (tkinter)
 
-class Application(tk.Frame):
+class Interface(tk.Tk):
     # Initialisation de la fenêtre
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-        self.master.geometry("1000x800")
+    def __init__(self):
+        self.path = "./camT.mp4"
+        tk.Tk.__init__(self)
+        self.create_main()
         self.label = None
         self.count = 0
         self.thread = None
         self.last_frame_time = time()
-        self.vs = FileVideoStream(path="./cam2.mp4").start()
+        self.vs = FileVideoStream(self.path).start()
         # self.vs = VideoStream(src=0).start()
         self.thread = threading.Thread(target=self.video_loop, args=())
         self.thread.start()
 
     # Création de boutons
-    def create_widgets(self):
-        self.selectFile1 = tk.Button(self)
-        self.selectFile1["text"] = "Select files"
-        self.selectFile1["command"] = self.video_loop
-        self.selectFile1.pack(side="bottom")
+    def create_main(self):
+        button1 = Button(
+            self,
+            text="Source",
+            width = 10,
+            command=partial(self.select_file),
+        )
+        button1.grid(row=1, column=0, padx=5, pady=5)
+
+        button2 = Button(
+            self,
+            text="Motion filter",
+            width = 10,
+            command=partial(self.open_motiom_filter),
+        )
+        button2.grid(row=1, column=1, padx=5, pady=5)
+
+        button3 = Button(
+            self,
+            text="Stats",
+            width = 10,
+            command=partial(self.video_loop),
+        )
+        button3.grid(row=1, column=2, padx=5, pady=5)
+
+        button4 = Button(
+            self,
+            text="Options",
+            width = 10,
+            command=partial(self.video_loop),
+        )
+        button4.grid(row=1, column=3, padx=5, pady=5)
+
+        button5 = Button(
+            self,
+            text="Help",
+            width = 10,
+            command=partial(self.video_loop),
+        )
+        button5.grid(row=1, column=4, padx=5, pady=5)
+
 
         # self.button_test = tk.Button(self, 
         #                             text="Send email",
         #                             command=partial(send_email))
         # self.button_test.pack(side="bottom")
-
+        
 
     # Fonction de sélection de fichier
-    def select_file(self, fileName=""):
-        if fileName == "":
-            fileName = askopenfilename()
-        return Image.open(fileName)
+    def select_file(self):
+        Tk().withdraw()
+        fileName = askopenfilename()
+        self.path = fileName
+        return
+
+    def open_motiom_filter(self):
+        motion_filter = tk.Toplevel()
+        motion_filter.geometry("+1000+100")
+        motion_filter.title("Motion Filter")
+
+        if self.label is None:
+            self.label = tk.Label(motion_filter, image=self.image)
+            self.label.grid(row=0, columnspan=5, padx=10,pady=10)
+        else:
+            self.label.configure(image=self.image)
+
+        self.label.image = self.image
+    
 
     def select_two_images(self):
         self.image1 = np.array(self.select_file("images\IPcam1.png"))
@@ -64,7 +116,7 @@ class Application(tk.Frame):
 
     def video_loop(self):
         while True:
-            while time() <= self.last_frame_time + 1 / 1000:
+            while time() <= self.last_frame_time + 1 / 30:
                 pass
             self.last_frame_time = time()
             self.frame = self.vs.read()
@@ -82,14 +134,14 @@ class Application(tk.Frame):
             self.display_image(image)
 
     def display_image(self, image1):
-        image = ImageTk.PhotoImage(image1)
+        self.image = ImageTk.PhotoImage(image1)
 
         if self.label is None:
-            self.label = tk.Label(image=image)
-            self.label.place(x=0, y=0)
+            self.label = tk.Label(image=self.image)
+            self.label.grid(row=0, columnspan=5, padx=10,pady=10)
         else:
-            self.label.configure(image=image)
-        self.label.image = image
+            self.label.configure(image=self.image)
+        self.label.image = self.image
 
         self.count += 1
 
@@ -104,6 +156,6 @@ class Application(tk.Frame):
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = Application(master=root)
-    app.mainloop()
+    root = Interface()
+    root.title("Motion detection")
+    root.mainloop()
