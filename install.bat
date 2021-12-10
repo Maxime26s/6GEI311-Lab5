edb07b2de15cedabf46888bb993ecd89c6d38f02
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2021-12-10 15:09:05 -0500

    updated .bat

--- install.bat
+++ install.bat
@@ -1,6 +1,9 @@
 REM Script d'initialisation de l'environnement
 REM Ajuster le chemin d'acces
 set path=%path%;c:\python3;c:\python3\scripts
+REM Récupère le projet du repository GitHub
+git clone https://github.com/Maxime26s/6GEI311-Lab5.git
+cd 6GEI311-Lab5
 REM Creer un repertoire
 mkdir venv
 cd venv
@@ -12,3 +15,5 @@ call .\Scripts\activate
 cd ..
 REM Installe les dépendances du fichier requirements.txt
 pip install -r requirements.txt
+REM Exécute le programme
+py main.py
